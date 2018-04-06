d75bd85ec7f3882ceef0094cf9bfd78e9220b9b9
Waterstrong <sqlin@thoughtworks.com>
2018-04-06 23:36:41 +0800

    [Shuiqiang] - init the spring boot project for ddd workshop

---
+++ src/main/java/com/ddd/tw/dddworkshop/DddWorkshopApplication.java
@@ -0,0 +1,12 @@
+package com.ddd.tw.dddworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddWorkshopApplication.class, args);
+	}
+}
