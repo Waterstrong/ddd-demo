d3b165b17c5027cd65502f707834d6114cb3cd31
Waterstrong <sqlin@thoughtworks.com>
2018-04-12 17:53:40 +0800

    [Shuiqiang] - add quote controller

--- src/main/java/com/ddd/tw/dddworkshop/webapi/policy/PolicyController.java
+++ src/main/java/com/ddd/tw/dddworkshop/webapi/policy/PolicyController.java
@@ -1,6 +1,9 @@
 package com.ddd.tw.dddworkshop.webapi.policy;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.ddd.tw.dddworkshop.policy.application.PolicyApplicationService;
 import com.ddd.tw.dddworkshop.policy.command.CarPolicyCommand;
@@ -11,10 +14,12 @@ public class PolicyController {
     @Autowired
     private PolicyApplicationService applicationService;
 
+    @ResponseStatus(CREATED)
     public String generateHomePolicy() {
         return applicationService.generatePolicy(HomePolicyCommand.builder().build());
     }
 
+    @ResponseStatus(CREATED)
     public String generateCarPolicy() {
         return applicationService.generatePolicy(CarPolicyCommand.builder().build());
     }
---
+++ src/main/java/com/ddd/tw/dddworkshop/webapi/policy/QuoteController.java
@@ -0,0 +1,34 @@
+package com.ddd.tw.dddworkshop.webapi.policy;
+
+import static org.springframework.http.HttpStatus.OK;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import com.ddd.tw.dddworkshop.policy.application.QuoteApplicationService;
+import com.ddd.tw.dddworkshop.policy.domain.CarDetails;
+import com.ddd.tw.dddworkshop.policy.domain.HomeDetails;
+import com.ddd.tw.dddworkshop.policy.domain.Quotation;
+
+@RestController
+public class QuoteController {
+    @Autowired
+    private QuoteApplicationService applicationService;
+
+    @ResponseStatus(OK)
+    public HttpEntity<Quotation> getHomePolicyQuote(String constructionMaterial, String buildingType, String bedroomsType) {
+        HomeDetails homeDetails = new HomeDetails(constructionMaterial, buildingType, bedroomsType);
+        Quotation quotation = applicationService.calculateQuote(homeDetails);
+        return new HttpEntity<>(quotation);
+    }
+
+    @ResponseStatus(OK)
+    public HttpEntity<Quotation> getCarPolicyQuote(String yearOfMake, String brand, String model,
+                                                   String parkingAddress, String kilosEachYear) {
+        CarDetails carDetails = new CarDetails(yearOfMake, brand, model, parkingAddress, kilosEachYear);
+        Quotation quotation = applicationService.calculateQuote(carDetails);
+        return new HttpEntity<>(quotation);
+    }
+
+}
